7daadb4d7e2542e5ad84816a1e36532f9070ef04
Jiheng Nie <Niejiheng@gmail.com>
2023-03-23 05:54:13 +0000

    Added in notes for chess and started on crud-app model

---
+++ crud-app/pages/index.tsx
@@ -0,0 +1,12 @@
+import CreateButton from "./components/createButton"
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold flex justify-center">
+        This is a test of my button!
+      </h1>
+      <CreateButton/>
+    </div>
+  )
+}
