90bb5c873a03c2182b9e1219a05ed9ae69bd6c92
Jiheng Nie <Niejiheng@gmail.com>
2023-03-27 18:48:12 +0000

    Reworked create new board function and started toying with piece movements

--- chess/pages/components/board.tsx
+++ chess/pages/components/board.tsx
@@ -1,23 +1,57 @@
-import React, { Component, useState} from 'react';
+import React, { Component} from 'react';
 
 interface BoardProps {
-  turn: string;
+  turn?: string;
 }
 
+type MyState = {
+  board?: string[][]
+}
 
-class Board extends Component<BoardProps> {
-  // constructors() {
-  //   this.handleClick = this.handleClick.bind(this);
-  // }
+class Board extends Component<BoardProps, MyState> {
+  constructor(props: BoardProps) {
+    super(props)
+
+    this.state = {
+      board: createNewArrayOfBoard()
+    }
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+  }
+
+
+ handleButtonClick(event) {
+   console.log(event.target.textContent)
+   console.log(event.target)
+}
 
   render() {
 
     const { turn } = this.props;
-    console.log(createNewBoard())
+    const { board } = this.state
+
+    const rowDivArray = []
+    for (let i = 0; i < 8; i++) {
+      const tempRow = []
+      for (let j = 0; j < 8; j++) {
+        tempRow.push(<div id={(j + i.toString())} onClick={this.handleButtonClick} className='basis-1/8'> {board![i][j] === 'Empty' ? 'Empty' : board![i][j]} </div>)
+      }
+      rowDivArray.push(tempRow)
+    }
+
+    const reversedRowDivArray = rowDivArray.reverse()
+    const htmlBoard = (
+      <div>
+        {reversedRowDivArray.map((rowDiv, index) => (
+          <div id={index.toString()} key={index} className='grid grid-cols-8 content-center text-center'>{rowDiv}</div>
+        ))}
+      </div>
+    );
+
+
     return (
       <>
         <h1 className='text-center text-2xl'>{turn}</h1>
-        <div>{createNewBoard()}</div>
+        <div>{htmlBoard}</div>
       </>
     );
   }
@@ -25,38 +59,19 @@ class Board extends Component<BoardProps> {
 
 export default Board;
 
-
-function createNewBoard() {
-  const nullArr: any[] = new Array(8).fill(null);
+function createNewArrayOfBoard() {
+  const empty = [] = new Array(8).fill('Empty');
   const whitePawn = [] = new Array(8).fill('w-pawn');
   const blackPawn = [] = new Array(8).fill('b-pawn');
   const arrayBoard = [
-    ['w-rook', 'w-knight', 'w-bishop', 'w-queen', 'w-king', 'w-bishop', 'w-knight','w-rook',],
+    ['w-rook', 'w-knight', 'w-bishop', 'w-queen', 'w-king', 'w-bishop', 'w-knight', 'w-rook',],
     whitePawn,
-    nullArr,
-    nullArr,
-    nullArr,
-    nullArr,
+    empty,
+    empty,
+    empty,
+    empty,
     blackPawn,
     ['b-rook', 'b-knight', 'b-bishop', 'b-queen', 'b-king', 'b-bishop', 'b-knight', 'b-rook',],
   ]
-
-  const rowDivArray = []
-  for (let i = 0; i < 8; i++) {
-    const tempRow = []
-    for (let j = 0; j < 8; j++) {
-      arrayBoard[i][j] === null ? tempRow.push(<div className='basis-1/8'> null </div>) : tempRow.push(<div className='basis-1/8'> {arrayBoard[i][j]} </div>)
-    }
-    rowDivArray.push(tempRow)
-  }
-
-const reversedRowDivArray = rowDivArray.reverse()
-const htmlBoard = (
-  <div>
-    {reversedRowDivArray.map((rowDiv, index) => (
-      <div key={index} className='grid grid-cols-8 content-center text-center'>{rowDiv}</div>
-    ))}
-  </div>
-);
-  return htmlBoard
+  return arrayBoard
 }
