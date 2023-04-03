5ff4f4c1e6c353e650fa898b54a6b3a195f01b2e
Jiheng Nie <Niejiheng@gmail.com>
2023-04-03 14:09:23 +0000

    Create a new potential board variable

--- chess/pages/components/board.tsx
+++ chess/pages/components/board.tsx
@@ -6,6 +6,7 @@ interface BoardProps {
 
 type MyState = {
   board?: string[][],
+  potentialBoard?: string[][],
   piece?: string | undefined,
   potentialSpaces: (number[] | null)[],
   currentLocation: number[]
@@ -17,6 +18,7 @@ class Board extends Component<BoardProps, MyState> {
 
     this.state = {
       board: createNewArrayOfBoard(),
+      potentialBoard: createNewArrayPotentialBoard(),
       piece: '',
       potentialSpaces: [[]],
       currentLocation: []
@@ -35,7 +37,7 @@ class Board extends Component<BoardProps, MyState> {
     const newLocation = [newY, newX]
     const newBoard = [...this.state.board!]
 
-    if (JSON.stringify(newLocation) === JSON.stringify(currentLocation) && checkItemInArray(potentialSpaces, newLocation)) {
+    if (JSON.stringify(newLocation) === JSON.stringify(currentLocation) && checkItemInPotentialSpaces(potentialSpaces, newLocation)) {
       // Setting Current space to null
       newBoard[yDirection][xDirection] = 'Empty'
       newBoard[newY][newX] = piece!
@@ -75,24 +77,24 @@ class Board extends Component<BoardProps, MyState> {
   render() {
     console.log(this.state)
     const { turn } = this.props;
-    const { board, piece, potentialSpaces} = this.state
+    const { board, potentialBoard} = this.state
 
     const rowDivArray = []
-    for (let i = 0; i < 8; i++) {
+    for (let y = 0; y < 8; y++) {
       const tempRow = []
-      for (let j = 0; j < 8; j++) {
-        const pieceContent = board![i][j] === 'Empty' ? 'Empty' : board![i][j].toString()
-        tempRow.push(<div id={`${i + j.toString()} ${pieceContent}`} onClick={this.handlePiecePotentialSpaces} className='basis-1/8' > {pieceContent} </div>)
+      for (let x = 0; x < 8; x++) {
+        const pieceContent = board![y][x] === 'Empty' ? 'Empty' : board![y][x].toString()
+        tempRow.push(<div id={`${y + x.toString()} ${pieceContent}`} onClick={this.handlePiecePotentialSpaces} className='basis-1/8' > {pieceContent} </div>)
       }
       rowDivArray.push(tempRow)
     }
 
     const potentialSpacesDivArray = []
-    for (let i = 0; i < 8; i++) {
+    for (let y = 0; y < 8; y++) {
       const tempRow = []
-      for (let j = 0; j < 8; j++) {
-        const pieceContent = board![i][j] === 'Empty' ? 'Empty' : board![i][j].toString()
-        tempRow.push(<div id={`${i + j.toString()} ${pieceContent}`} onClick={this.handlePiecePotentialSpaces} className='basis-1/8' > {pieceContent} </div>)
+      for (let x = 0; x < 8; x++) {
+        const pieceContent = potentialBoard![y][x] === 'Empty' ? 'Empty' : potentialBoard![y][x].toString()
+        tempRow.push(<div id={`${y + x.toString()} ${pieceContent}`} className='basis-1/8' > {pieceContent} </div>)
       }
       potentialSpacesDivArray.push(tempRow)
     }
@@ -103,10 +105,18 @@ class Board extends Component<BoardProps, MyState> {
       </div>
     );
 
+    const htmlPotentialSpaces = (
+      <div className='grid grid-cols-8 content-center text-center'>
+        {potentialSpacesDivArray}
+      </div>
+    )
+
     return (
       <>
         <h1 className='text-center text-2xl'>{turn}</h1>
-        <div onClick={this.handlePieceClick} >{htmlBoard}</div>
+        <div>{htmlBoard}</div>
+        <h1 className='text-center text-2xl'>Potential Moves</h1>
+        <div>{htmlPotentialSpaces}</div>
       </>
     );
   }
@@ -134,6 +144,28 @@ function createNewArrayOfBoard() {
   return arrayBoard
 }
 
+function createNewArrayPotentialBoard() {
+  const empty1 = [] = new Array(8).fill('Empty');
+  const empty2 = [] = new Array(8).fill('Empty');
+  const empty3 = [] = new Array(8).fill('Empty');
+  const empty4 = [] = new Array(8).fill('Empty');
+  const empty5 = [] = new Array(8).fill('Empty');
+  const empty6 = [] = new Array(8).fill('Empty');
+  const empty7 = [] = new Array(8).fill('Empty');
+  const empty8 = [] = new Array(8).fill('Empty');
+  const arrayBoard = [
+    empty1,
+    empty2,
+    empty3,
+    empty4,
+    empty5,
+    empty6,
+    empty7,
+    empty8
+  ]
+  return arrayBoard
+}
+
 function checkCordsWithinBoard(xDirection: number, yDirection: number ) {
   if (xDirection >= 0 && xDirection <= 7 && yDirection >= 0 && yDirection <= 7 ) {
     return true
@@ -142,7 +174,7 @@ function checkCordsWithinBoard(xDirection: number, yDirection: number ) {
   }
 }
 
-function checkItemInArray(arr: (number[] | null)[], item: number[]) {
+function checkItemInPotentialSpaces(arr: (number[] | null)[], item: number[]) {
   for (let index in arr) {
     if (JSON.stringify(item) === JSON.stringify(arr[index])) {
       return true
